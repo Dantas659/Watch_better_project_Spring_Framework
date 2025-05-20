440c04f3ba990ee05336af4e4a28a9fa7897ac50
Lívia Dantas <liviadantas659@gmail.com>
2025-05-20 17:05:13 -0300

    Enhance Menu functionality to display season details and refactor Principal to utilize Menu for user interaction

--- watch_better/src/main/java/br/com/watch_better/principal/Menu.java
+++ watch_better/src/main/java/br/com/watch_better/principal/Menu.java
@@ -1,11 +1,17 @@
 package br.com.watch_better.principal;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import br.com.watch_better.model.SeasonData;
+import br.com.watch_better.model.SerieData;
 import br.com.watch_better.service.ApiConsumer;
+import br.com.watch_better.service.dataConverter.ConversionService;
 
 public class Menu {
     private ApiConsumer apiConsumer = new ApiConsumer();
+    private ConversionService conversionService = new ConversionService();
     private Scanner scanner = new Scanner(System.in);
     private static final String ENDPOINT = "http://www.omdbapi.com/?t=";
     private static final String API_KEY = "&apikey=14c1e91b";
@@ -16,8 +22,20 @@ public class Menu {
         System.out.println("Welcome to Watch Better!");
         System.out.println("What do you want to see?");
         String nameSerie = scanner.nextLine();
-        String json = apiConsumer.getData(ENDPOINT + nameSerie.replace("", "+") + API_KEY);   
-        System.out.println(json); 
+        String json = apiConsumer.getData(ENDPOINT + nameSerie.replace(" ", "+") + API_KEY);
+        SerieData data = conversionService.dataFetcher(json, SerieData.class);   
+        System.out.println(data); 
+
+        List<SeasonData> seasons = new ArrayList<>();
+        for(int i = 1; i <= data.totalSeasons(); i++) {
+            json = apiConsumer.getData(ENDPOINT + nameSerie.replace(" ", "+") + "&Season=" + i + API_KEY);
+            SeasonData seasonData = conversionService.dataFetcher(json, SeasonData.class);
+            seasons.add(seasonData);
+        }
+        seasons.forEach(System.out::println);
     }
+
+
+   
     
 }
--- watch_better/src/main/java/br/com/watch_better/principal/Principal.java
+++ watch_better/src/main/java/br/com/watch_better/principal/Principal.java
@@ -1,16 +1,7 @@
 package br.com.watch_better.principal;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-
-import br.com.watch_better.model.EpisodeData;
-import br.com.watch_better.model.SeasonData;
-import br.com.watch_better.model.SerieData;
-import br.com.watch_better.service.ApiConsumer;
-import br.com.watch_better.service.dataConverter.ConversionService;
 
 public class Principal implements CommandLineRunner {
     public static void main(String[] args) {
@@ -20,25 +11,10 @@ public class Principal implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        ApiConsumer apiConsumer = new ApiConsumer();
-        String json = apiConsumer.getData("http://www.omdbapi.com/?t=gilmore+girls&apikey=14c1e91b");
-        ConversionService conversionService = new ConversionService();
-        SerieData data = conversionService.dataFetcher(json, SerieData.class);
-        System.out.println(data);
+        Menu menu = new Menu();
+        menu.showMenu();
+  
 
-        json = apiConsumer.getData("http://www.omdbapi.com/?t=gilmore+girls&Season=1&Episode=1&apikey=14c1e91b");
-        EpisodeData episodeData = conversionService.dataFetcher(json, EpisodeData.class);
-        System.out.println(episodeData);
-
-        List<SeasonData> seasons = new ArrayList<>();
-
-        for(int i = 1; i <= data.totalSeasons(); i++) {
-            json = apiConsumer.getData("http://www.omdbapi.com/?t=gilmore+girls&Season=" + i + "&apikey=14c1e91b");
-            SeasonData seasonData = conversionService.dataFetcher(json, SeasonData.class);
-            seasons.add(seasonData);
-            System.out.println(seasonData);
-
-        }
 
     }
 }
